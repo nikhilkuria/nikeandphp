e0729caae3cba221a4069a5b414db38565303d36
nikhilkuria <nikhilkuria@gmail.com>
2017-02-23 06:10:09 +0530

    adding a new structure to the test classes

---
+++ tests/LoginTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikhilkuria
+ * Date: 23/02/17
+ * Time: 6:05 AM
+ */
+
+use PHPUnit\Framework\TestCase;
+
+final class LoginTest extends TestCase{
+    public function testOnePlusOne() {
+        $this->assertEquals(1+1,2);
+    }
+}
